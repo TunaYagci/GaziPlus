25fca140dc60edb23fc1e987c27af168f6cdc953
tnaygc <yagcituna@gmail.com>
2015-10-19 22:17:54 +0300

    - yemek widget push

--- app/src/main/java/com/hp2m/GaziPlus/widget_yemek.java
+++ app/src/main/java/com/hp2m/GaziPlus/widget_yemek.java
@@ -1,11 +1,17 @@
 package com.hp2m.GaziPlus;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -15,9 +21,85 @@ import de.greenrobot.event.EventBus;
  * Implementation of App Widget functionality.
  */
 public class widget_yemek extends AppWidgetProvider {
+    private static final String SYNC_CLICK = "myOnClickTag";
     final EventBus bus = EventBus.getDefault();
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+    private static int giveMeTheDay() {
+        // scroll to day
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        switch (day) {
+            case Calendar.MONDAY:
+                return 1;
+            case Calendar.TUESDAY:
+                return 2;
+            case Calendar.WEDNESDAY:
+                return 3;
+            case Calendar.THURSDAY:
+                return 4;
+            case Calendar.FRIDAY:
+                return 5;
+            default:
+                return 6;
+
+        }
+    }
+
+    protected PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        if (SYNC_CLICK.equals(intent.getAction())) {
+            //your onClick action is here
+            if (isNetworkAvailable(context)) {
+                Toast.makeText(context, "Güncelleniyor", Toast.LENGTH_SHORT).show();
+                // update here
+            } else {
+                String gun, yemek1, yemek2, yemek3, yemek4;
+
+                YemekDB db = new YemekDB(context);
+                if (db.getYemekSayisi() != 0) {
+                    int day = giveMeTheDay();
+                    if (day != 6) { // the value "6" returns the weekends
+                        gun = db.fetchMeMyFood(day).get(0);
+                        yemek1 = db.fetchMeMyFood(day).get(1);
+                        yemek2 = db.fetchMeMyFood(day).get(2);
+                        yemek3 = db.fetchMeMyFood(day).get(3);
+                        yemek4 = db.fetchMeMyFood(day).get(4);
+                    } else {
+                        gun = db.fetchMeMyFood(5).get(0);
+                        yemek1 = db.fetchMeMyFood(5).get(1);
+                        yemek2 = db.fetchMeMyFood(5).get(2);
+                        yemek3 = db.fetchMeMyFood(5).get(3);
+                        yemek4 = db.fetchMeMyFood(5).get(4);
+                    }
+
+                } else {
+                    gun = "Yemek listesi bulunamadý";
+                    yemek1 = "-";
+                    yemek2 = "-";
+                    yemek3 = "-";
+                    yemek4 = "-";
+                }
+
+                // Construct the RemoteViews object
+                RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_yemek);
+                views.setTextViewText(R.id.widgetyemek_gun, gun);
+                views.setTextViewText(R.id.widgetyemek_1, yemek1);
+                views.setTextViewText(R.id.widgetyemek_2, yemek2);
+                views.setTextViewText(R.id.widgetyemek_3, yemek3);
+                views.setTextViewText(R.id.widgetyemek_4, yemek4);
+            }
+        }
+    }
+
+    public void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
         String gun, yemek1, yemek2, yemek3, yemek4;
@@ -25,14 +107,13 @@ public class widget_yemek extends AppWidgetProvider {
         YemekDB db = new YemekDB(context);
         if (db.getYemekSayisi() != 0) {
             int day = giveMeTheDay();
-            if (day != 6){ // the value "6" returns the weekends
+            if (day != 6) { // the value "6" returns the weekends
                 gun = db.fetchMeMyFood(day).get(0);
                 yemek1 = db.fetchMeMyFood(day).get(1);
                 yemek2 = db.fetchMeMyFood(day).get(2);
                 yemek3 = db.fetchMeMyFood(day).get(3);
                 yemek4 = db.fetchMeMyFood(day).get(4);
-            }
-            else {
+            } else {
                 gun = db.fetchMeMyFood(5).get(0);
                 yemek1 = db.fetchMeMyFood(5).get(1);
                 yemek2 = db.fetchMeMyFood(5).get(2);
@@ -56,29 +137,11 @@ public class widget_yemek extends AppWidgetProvider {
         views.setTextViewText(R.id.widgetyemek_3, yemek3);
         views.setTextViewText(R.id.widgetyemek_4, yemek4);
 
+        // sync click
+        views.setOnClickPendingIntent(R.id.sync_button, getPendingSelfIntent(context, SYNC_CLICK));
+
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
-
-    private static int giveMeTheDay() {
-        // scroll to day
-        Calendar calendar = Calendar.getInstance();
-        int day = calendar.get(Calendar.DAY_OF_WEEK);
-        switch (day) {
-            case Calendar.MONDAY:
-                return 1;
-            case Calendar.TUESDAY:
-                return 2;
-            case Calendar.WEDNESDAY:
-                return 3;
-            case Calendar.THURSDAY:
-                return 4;
-            case Calendar.FRIDAY:
-                return 5;
-            default:
-                return 6;
-
-        }
     }
 
     @Override
@@ -86,36 +149,33 @@ public class widget_yemek extends AppWidgetProvider {
         super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
 
-        int minWidth = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
-        /*int maxWidth = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH);
-        int minHeight = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
-        int maxHeight = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT);
-*/
+            int minWidth = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+            int maxWidth = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH);
+            int minHeight = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
+            int maxHeight = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT);
 
-        //Log.i("tuna", minWidth + " " + maxWidth + " " + minHeight + " " + maxHeight);
 
-        int unit = 3;
-        float size = 8f;
+            Log.i("tuna", minWidth + " " + maxWidth + " " + minHeight + " " + maxHeight);
 
-        switch (minWidth){
-            case 128:
+            int unit = 3;
+            float size = 8f;
+
+            if (minWidth < 200) {
                 unit = 3;
-                size = 7f;
-                break;
-            case 200:
+                size = 6f;
+            } else if (minWidth < 250) {
                 unit = 3;
-                break;
-            case 272:
+                size = 8f;
+            } else if (minWidth < 320) {
                 unit = 3;
                 size = 9f;
-                break;
-            case 344:
+            } else if (minWidth < 400) {
                 unit = 3;
                 size = 9f;
-                break;
-        }
+            }
 
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_yemek);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_yemek);
 
             views.setTextViewTextSize(R.id.widgetyemek_gun, unit, size);
             views.setTextViewTextSize(R.id.widgetyemek_1, unit, size);
@@ -126,6 +186,13 @@ public class widget_yemek extends AppWidgetProvider {
         }
 
 
+    }
+
+    private boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
     @Override
@@ -141,6 +208,7 @@ public class widget_yemek extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+        Log.i("tuna", "onEnabled");
     }
 
     @Override
